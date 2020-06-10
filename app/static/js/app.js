7d69bad7869e0e253fe62c3d8b39f2394c8ce0f3
GiselleTodd <43821673+GiselleTodd@users.noreply.github.com>
2020-06-09 20:09:30 -0500

    add vue components to app.js and create landing.html

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,8 +1,29 @@
 /* Add your Application JavaScript */
-console.log('this is some JavaScript code');
-
-function notify() {
-  alert('in here I will do something');
-}
+// console.log('this is some JavaScript code');
+//
+// function notify() {
+//   alert('in here I will do something');
+// }
 
 // notify();
+const Landing = Vue.component('landing-page',{
+  template:`
+  `,
+  data: function (){
+    return { }
+  }
+})
+
+
+const router = new VueRouter({
+  mode:'history',
+  routes: [
+    {path:'/', componenet: Home},
+    {path:'/landing', component:Landing}
+  ]
+})
+
+let app = new Vue({
+    el: '#app',
+    router
+});
